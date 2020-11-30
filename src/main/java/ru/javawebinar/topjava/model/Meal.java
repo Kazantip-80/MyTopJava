0522dbe932c3bdc72233b62da5821c0d831b533d
Max <iatemax@mail.ru>
2020-11-30 14:46:05 +0300

    HW04 all

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,19 +1,45 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@NamedQueries({
+        @NamedQuery(name = Meal.BY_ID,query = "SELECT m FROM Meal m WHERE m.id=:id AND m.user.id=:user_id"),
+        @NamedQuery(name = Meal.DELETE,query = "DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:user_id"),
+        @NamedQuery(name = Meal.ALL_SORTED,query = "SELECT m FROM Meal m WHERE m.user.id=:user_id ORDER BY m.dateTime DESC"),
+        @NamedQuery(name = Meal.BY_BETWEEN,query = "SELECT m FROM Meal m WHERE m.user.id=?1  AND m.dateTime>=?2 AND m.dateTime<=?3 ORDER BY m.dateTime DESC")
+})
+
+@Entity
+@Table(name = "meals", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id","date_time"}, name = "meals_unique_user_datetime_idx")})
 public class Meal extends AbstractBaseEntity {
+
+    public static final String DELETE = "Meal.delete";
+    public static final String BY_ID = "Meal.getByID";
+    public static final String ALL_SORTED = "Meal.getAllSorted";
+    public static final String BY_BETWEEN = "Meal.getByBetween";
+
+
+
+    @Column(name = "date_time", nullable = false)
+    @NotNull
     private LocalDateTime dateTime;
 
+    @Column(name = "description", nullable = false)
+    @NotBlank
     private String description;
 
+    @Column(name = "calories", nullable = false)
+    @NotNull
     private int calories;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @CollectionTable(name = "users", joinColumns = @JoinColumn(name = "id"))
+  //  @Column(name = "user_id")
+    @ManyToOne(fetch = FetchType.EAGER)
     private User user;
 
     public Meal() {
@@ -29,6 +55,7 @@ public class Meal extends AbstractBaseEntity {
         this.description = description;
         this.calories = calories;
     }
+
 
     public LocalDateTime getDateTime() {
         return dateTime;
--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,7 +1,14 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.AfterClass;
+import org.slf4j.Logger;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
@@ -12,6 +19,9 @@ import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
@@ -28,6 +38,46 @@ public class MealServiceTest {
     @Autowired
     private MealService service;
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    private static final Logger log = LoggerFactory.getLogger(MealServiceTest.class);
+    private long start;
+    private double lasted;
+    private String resultTest;
+
+    private static final Map<String,Double> mapTimeTests=new ConcurrentHashMap<>();
+
+    @Rule
+    public TestWatcher watcher = new TestWatcher() {
+
+        @Override
+        protected void succeeded(Description description) {
+            resultTest="success";
+        }
+
+        @Override
+        protected void failed(Throwable e, Description description) {
+            resultTest="failed";
+        }
+
+        @Override
+        protected void starting(Description description) {
+          //  System.out.println("starting");
+         //   throw new IllegalStateException();
+            start = System.currentTimeMillis();
+        }
+
+        @Override
+        protected void finished(Description description) {
+            long finish=System.currentTimeMillis();
+            lasted=(finish-start)/1000.0;
+            log.info("Test \"{}\" {} , lasted for {} sec",description.getMethodName(),resultTest,lasted);
+            mapTimeTests.put(description.getMethodName(),lasted);
+        }
+    };
+
+
     @Test
     public void delete() throws Exception {
         service.delete(MEAL1_ID, USER_ID);
@@ -37,6 +87,12 @@ public class MealServiceTest {
     @Test(expected = NotFoundException.class)
     public void deleteNotFound() throws Exception {
         service.delete(1, USER_ID);
+    }
+
+    @Test
+    public void deleteNotFoundCheckRule(){
+        thrown.expect(NotFoundException.class);
+        service.delete(99, USER_ID);
     }
 
     @Test(expected = NotFoundException.class)
@@ -51,6 +107,15 @@ public class MealServiceTest {
         newMeal.setId(created.getId());
         assertMatch(newMeal, created);
         assertMatch(service.getAll(USER_ID), newMeal, MEAL6, MEAL5, MEAL4, MEAL3, MEAL2, MEAL1);
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        System.out.println("----------------------------------");
+        mapTimeTests
+                .forEach((key, value) -> System.out.println(String.format("Test method \"%s\" duration: %f sec", key
+                        , value)));
+        System.out.println("----------------------------------");
     }
 
     @Test
